7bdf881d8495ec785143664976fb11073ad64e79
julia-sand <73934921+julia-sand@users.noreply.github.com>
2026-01-09 15:45:24 +0200

    add weights initialisation

--- gbm.c
+++ gbm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdio.h>
 #include <time.h>
 #include <math.h>
 #include <stdlib.h>
@@ -6,6 +7,10 @@
 /*problem variables*/
 #define mu 1
 #define sigma 1 
+#define neurons_in 8
+#define neurons_out 1
+#define batch 10
+
 
 double generate_uniform()
 /*returns a U(0,1) random variable*/
@@ -13,6 +18,13 @@ double generate_uniform()
     return (double)rand() / (double)RAND_MAX;
 }
 
+
+double generate_uniform_shifted(double lim)
+/*returns a U(-limit,limit) random variable*/
+{   
+    
+    return (double)((2*lim)*(double)generate_uniform()) - lim;
+}
 
 double nrand()
 /*takes two uniform random variables and generates a normal random variable using the box muller transform*/
@@ -34,7 +46,7 @@ double integrate_gbm(double x0, int tsteps, double dt)
     return xs[tsteps-1];
 }
 
-void generate_training_data(int batch, double *X0, double *XT)
+void generate_training_data(double *X0, double *XT)
 {
     srand(time(NULL)); // seed with currtime
     
@@ -44,18 +56,35 @@ void generate_training_data(int batch, double *X0, double *XT)
     {
         X0[i] = generate_uniform();
         XT[i] = integrate_gbm(X0[i],10,dt);
-        printf("%f\n",XT[i]);
-    }
-    
+        //printf("%f\n",XT[i]);
+    }   
 }
 
+void initialise_network_weights(double W1[][neurons_out])
+{
+    srand(time(NULL)); // seed with currtime
+    double lim_glorot = sqrt(6 / (neurons_in + neurons_out)); 
+    
+   for (int i=0; i<neurons_in; i++) //use glorot uniform, ie sample initial weights from uniform
+    {
+        for (int j=0; j<neurons_out; j++)
+            {
+            W1[i][j] = generate_uniform_shifted(lim_glorot);
+    }}
+}
+
+
+//driver
 void main()
 {
-    int batch = 20;
-    double X0[batch] ; //initial 
-    double XT[batch] ; //final
+    double X0[batch] ={0} ; //initial 
+    double XT[batch] ={0} ; //final
+
+    double W1[neurons_in][neurons_out] ={{0}} ; //layer weights
+
+    double bias[neurons_in] ; //layer bias
+
+    initialise_network_weights(W1);
     
-    //pointer to 
-    generate_training_data(batch, X0, XT);
-    printf("%f\n",XT[batch-1]);
+    generate_training_data(X0, XT);
 }
